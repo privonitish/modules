77aa2cb8296f4829b24e5333e5fd9a5e50b40321
privonitish <73235658+privonitish@users.noreply.github.com>
2020-12-07 18:33:53 +0530

    Update ec2.tf

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-08d9a394ac1c2994c"
-  instance_type = "t2.micro"
+  instance_type = "${var.itype}"
   subnet_id = "subnet-02d633f9f90d3817a"
   user_data     = <<-EOT
     #!/bin/bash
